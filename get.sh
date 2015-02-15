d447f3ee01efd72d694636ad3fc5234d261573a9
kleinfreund <phrudloff@gmail.com>
2015-02-15 16:46:10 +0100

    Add small shell script to get the files

---
+++ get.sh
@@ -0,0 +1,8 @@
+DEST=~/dev/dotfiles/
+
+cp ~/.gitignore_global "$DEST"
+cp ~/.gitconfig "$DEST"
+cp ~/.bashrc "$DEST"
+cp ~/.bash_aliases "$DEST"
+cp ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings "$DEST"
+cp ~/.config/sublime-text-3/Packages/User/"Default (Linux).sublime-keymap" "$DEST"
