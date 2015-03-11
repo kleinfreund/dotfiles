adbb009d19a1647fd269ffcedb1bb07438d17452
kleinfreund <phrudloff@gmail.com>
2015-03-11 11:56:56 +0100

    Update get.sh script

--- get.sh
+++ get.sh
@@ -1,9 +1,18 @@
+# Shell script to copy important configuration files on my Ubuntu environment
+# to this repository.
+
+# Location of the repository
 DEST=~/dev/dotfiles/
 
-cp ~/.gitignore_global "$DEST"
-cp ~/.gitconfig "$DEST"
+# Bash
 cp ~/.bashrc "$DEST"
 cp ~/.bash_aliases "$DEST"
+
+# Git
+cp ~/.gitignore_global "$DEST"
+cp ~/.gitconfig "$DEST"
+
+# Sublime Text
 cp ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings "$DEST"
 cp ~/.config/sublime-text-3/Packages/User/"Default (Linux).sublime-keymap" "$DEST"
 cp ~/.config/sublime-text-3/Packages/User/"Package Control.sublime-settings" "$DEST"
