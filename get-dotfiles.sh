36a15e0030eaac368daee0c4d88c17291f82bcf8
kleinfreund <phrudloff@gmail.com>
2015-03-16 16:49:13 +0100

    Overhauling the shell script to be OS specific. What a mess, still. :)

---
+++ get-dotfiles.sh
@@ -0,0 +1,63 @@
+# Shell script to copy important configuration files on my Ubuntu environment
+# to this repository.
+
+# Local location of the repository
+REPO_PATH=$HOME/dev/repos/dotfiles/
+
+
+
+# Detect OS (OS X, Linux or Windows)
+if [ "$(uname)" == "Darwin" ]; then
+    echo "Detected system: OS X"
+
+    OS_PATH="osx/"
+
+    ST_DIR="$HOME/Library/Application Support/Sublime Text 3/Packages/User/"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo "Detected system: Linux"
+
+    OS_PATH="linux/"
+
+    ST_DIR="$HOME/.config/sublime-text-3/Packages/User/"
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    echo "Detected system: Windows"
+
+    OS_PATH="win/"
+
+    ST_DIR="$HOME/AppData/Roaming/Sublime Text 3/Packages/User/"
+fi
+
+# Adjust destination paths so the files are separated by OS
+BASH_DEST="${REPO_PATH}${OS_PATH}bash/"
+GIT_DEST="${REPO_PATH}${OS_PATH}git/"
+RUBY_DEST="${REPO_PATH}${OS_PATH}ruby/"
+ST_DEST="${REPO_PATH}${OS_PATH}sublime-text/"
+
+# Create necessary directories if they don't exist
+mkdir -p $BASH_DEST
+mkdir -p $GIT_DEST
+mkdir -p $RUBY_DEST
+mkdir -p $ST_DEST
+
+
+
+### Copying the files to the repository
+
+# Bash
+cp $HOME/.bashrc "$BASH_DEST"
+cp $HOME/.bash_aliases "$BASH_DEST"
+
+# Git
+cp $HOME/.gitignore_global "$GIT_DEST"
+cp $HOME/.gitconfig "$GIT_DEST"
+
+# Ruby/RubyGems
+cp $HOME/.gemrc "$RUBY_DEST"
+
+# Sublime Text (Windows)
+cp "${ST_DIR}Preferences.sublime-settings" "$ST_DEST"
+cp "${ST_DIR}Default (Windows).sublime-keymap" "$ST_DEST"
+cp "${ST_DIR}Package Control.sublime-settings" "$ST_DEST"
+cp "${ST_DIR}For Loop (range).sublime-snippet" "$ST_DEST"
+
+echo "Completed."
---
+++ linux/bash/.bashrc
@@ -0,0 +1,10 @@
+# Alias definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them here directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+# Jekyll environment
+export JEKYLL_ENV=local
