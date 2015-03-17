8cc8a6344de486e745729d19ec3c2f41803ec7e0
kleinfreund <phrudloff@gmail.com>
2015-03-17 13:43:15 +0100

    Get npm configuration as well

--- get-dotfiles.sh
+++ get-dotfiles.sh
@@ -2,55 +2,61 @@
 # to this repository.
 
 # Local location of the repository
-REPO_PATH=$HOME/dev/repos/dotfiles/
+REPO_PATH="$HOME/dev/repos/dotfiles/"
 
 
 
 # Detect OS (OS X, Linux or Windows)
 if [ "$(uname)" == "Darwin" ]; then
-    echo "Detected system: OS X"
+    echo "System: OS X"
 
     OS_PATH="osx/"
 
     ST_DIR="$HOME/Library/Application Support/Sublime Text 3/Packages/User/"
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    echo "Detected system: Linux"
+    echo "System: Linux"
 
     OS_PATH="linux/"
 
     ST_DIR="$HOME/.config/sublime-text-3/Packages/User/"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-    echo "Detected system: Windows"
+    echo "System: Windows"
 
     OS_PATH="win/"
 
     ST_DIR="$HOME/AppData/Roaming/Sublime Text 3/Packages/User/"
+    NPM_DIR="$HOME/AppData/Roaming/npm/node_modules/npm/"
 fi
 
 # Adjust destination paths so the files are separated by OS
 BASH_DEST="${REPO_PATH}${OS_PATH}bash/"
 GIT_DEST="${REPO_PATH}${OS_PATH}git/"
+NPM_DEST="${REPO_PATH}${OS_PATH}npm/"
 RUBY_DEST="${REPO_PATH}${OS_PATH}ruby/"
 ST_DEST="${REPO_PATH}${OS_PATH}sublime-text/"
 
 # Create necessary directories if they don't exist
 mkdir -p $BASH_DEST
 mkdir -p $GIT_DEST
+mkdir -p $NPM_DEST
 mkdir -p $RUBY_DEST
 mkdir -p $ST_DEST
 
 
 
 # Bash
-cp $HOME/.bashrc "$BASH_DEST"
-cp $HOME/.bash_aliases "$BASH_DEST"
+cp "$HOME/.bashrc" "$BASH_DEST"
+cp "$HOME/.bash_aliases" "$BASH_DEST"
 
 # Git
-cp $HOME/.gitignore_global "$GIT_DEST"
-cp $HOME/.gitconfig "$GIT_DEST"
+cp "$HOME/.gitignore_global" "$GIT_DEST"
+cp "$HOME/.gitconfig" "$GIT_DEST"
+
+# Node.js/npm
+cp "${NPM_DIR}.npmrc" "$NPM_DEST"
 
 # Ruby/RubyGems
-cp $HOME/.gemrc "$RUBY_DEST"
+cp "$HOME/.gemrc" "$RUBY_DEST"
 
 # Sublime Text (Windows)
 cp "${ST_DIR}Preferences.sublime-settings" "$ST_DEST"
