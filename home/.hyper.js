da105a4d66c3c8c20c956e458c565be2fa873bfc
kleinfreund <phrudloff@gmail.com>
2016-12-06 08:20:07 +0100

    Update files

--- home/.hyper.js
+++ home/.hyper.js
@@ -81,7 +81,9 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: [
+    'hyper-blink'
+  ],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
