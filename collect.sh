c36d4020d14b694ff56e1d69e4ec4866eb3b370c
kleinfreund <philipprudloff@fastmail.com>
2019-08-25 16:25:10 +0200

    Slight updates to the collect script

--- collect.sh
+++ collect.sh
@@ -1,61 +1,54 @@
 #!/bin/bash
 
-################################################################################
-#
-# Shell script to copy important configuration files from the current
-# environment to this repository.
-#
-################################################################################
+# Change directory to the location of this script.
+# This way, it can be executed from an arbitrary location.
+dotfiles_dir="$(dirname "${BASH_SOURCE}")";
+cd $dotfiles_dir;
 
 # Exit as soon as a command fails
-set -e
+set -e;
 
 # Accessing an empty variable will yield an error
-set -u
-
-# Assumption: The dotfiles directory resides in $HOME
-repo_dir="$HOME/dotfiles"
-
-# Go to the dotfiles directory.
-# This allows the script to be executed from elsewhere
-cd $repo_dir
+set -u;
 
 
 
 # Store kernel name in variable:
-unamestr=$(uname -s)
+unamestr=$(uname -s);
 
 # Detect OS (Linux or Windows)
-printf "Detecting OS: "
+printf "Detecting OS: ";
 if [[ "$unamestr" == "Linux" ]]; then
-  printf "Linux"
+  printf "Linux";
 
-  vscode_dir="$HOME/.config/Code/User"
-  albert_dir="$HOME/.config/albert"
+  vscode_dir="$HOME/.config/Code/User";
+  albert_dir="$HOME/.config/albert";
 elif [[ "$unamestr" == "MINGW32_NT"* || "$unamestr" == "MINGW64_NT"* ]]; then
-  printf "Windows"
+  printf "Windows";
 
-  vscode_dir="$HOME/AppData/Roaming/Code/User"
+  vscode_dir="$HOME/AppData/Roaming/Code/User";
 else
-  echo "Could not detect operating system. Aborting."
-  exit 2
+  echo "Could not detect operating system. Aborting.";
+  exit 2;
 fi
-echo
+echo;
 
-echo "Copying Visual Studio Code files ..."
-vscode_files="snippets/ keybindings.json settings.json"
-mkdir -p "$repo_dir/code"
+echo "Copying Visual Studio Code files ...";
+vscode_files="snippets/ keybindings.json settings.json";
+mkdir -p "$dotfiles_dir/code";
 for file in $vscode_files; do
-  [ -f "${vscode_dir}/$file" ] && cp -ur "${vscode_dir}/$file" "$repo_dir/code"
-  echo "  ${vscode_dir}/$file"
+  [ -f "${vscode_dir}/$file" ] && cp -ur "${vscode_dir}/$file" "$dotfiles_dir/code";
+  echo "  ${vscode_dir}/$file";
 done
 
-echo "Copying Albert files ..."
-albert_files="org.albert.extension.snippets/ org.albert.extension.websearch/ albert.conf"
-mkdir -p "$repo_dir/albert"
+echo "Copying Albert files ...";
+albert_files="org.albert.extension.snippets/ org.albert.extension.websearch/ albert.conf";
+mkdir -p "$dotfiles_dir/albert";
 for file in $albert_files; do
-  [ -f "${albert_dir}/$file" ] && cp -ur "${albert_dir}/$file" "$repo_dir/albert"
-  echo "  ${albert_dir}/$file"
+  [ -f "${albert_dir}/$file" ] && cp -ur "${albert_dir}/$file" "$dotfiles_dir/albert";
+  echo "  ${albert_dir}/$file";
 done
 
-echo "Completed."
+unset dotfiles_dir;
+
+echo "Completed.";
